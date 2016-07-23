edb830a4c0de85ab8bacde5bc57e4627fedb7029
Tom Yotwongjai <travel3.biz@gmail.com>
2016-07-23 13:42:12 +0000

    pages style

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,11 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  def index
+    @palces= Place.all
 end
+
+  def new
+    @places = Place.new
+
+   end
+end
+  
--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -3,8 +3,12 @@ class PlacesController < ApplicationController
  def index
   @places = Place.order("name").page(params[:page]).per_page(1)
  end
+
+
+def new
+  @palces = Place.new
+
 end
 
-
-
+end
 
