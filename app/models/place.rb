86d32d4481be02567a3fa38c12b2c8c5dd9ee70d
Tom Yotwongjai <travel3.biz@gmail.com>
2016-07-28 04:00:13 +0000

    styling sign-in page

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,7 +1,4 @@
 class Place < ActiveRecord::Base
-def self.search(search, page)
-   paginate :per_page => 2, :page => page,
-           :conditions => ['name like ?', "%#{search}%"],
-           :order => 'name'
-end
-end
+  
+  belongs_to :user
+ end
