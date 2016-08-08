e7dc742b1834030f258396a15332d510d8a7420e
Tom Yotwongjai <travel3.biz@gmail.com>
2016-08-08 05:15:55 +0000

    Photo

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,7 +1,9 @@
 class NotificationMailer < ActionMailer::Base
   default from: "no-reply@nomsterapp.com"
 
-  def comment_added
+  def comment_added(comment)
+    @place = comment.place
+    @place_owner = @place.user
     mail(to: "travel3.biz@gmail.com",
        Subject: "A comment has been added to your place")
   end
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,6 +1,4 @@
 class Photo < ActiveRecord::Base
  mount_uploader :picture, PictureUploader
-   belongs_to :user
-   belongs_to :place
 end
   
