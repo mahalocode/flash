008781cc3c57badc50421ffe0fa829e56cba2648
Tom Yotwongjai <travel3.biz@gmail.com>
2016-08-11 00:21:28 +0000

    map

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,8 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+ Geocoder.configure(:timeout => 1000)
+
 module Nomster
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
